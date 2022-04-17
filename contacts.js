9ef40075982e0eab3ff923b4c97869966ff192be
ValiaStohnii <valia19961018@gmail.com>
2022-04-17 18:00:44 +0300

    add file

---
+++ contacts.js
@@ -0,0 +1,45 @@
+const fs = require('fs/promises') 
+const contactsPath = require('./contactsPath')
+
+const { nanoid } = require('nanoid');
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find(item => item.id === contactId);
+  if (!contact) {
+    return null;
+  }
+  return contact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex(item => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const [removeContact] = contacts.splice(idx, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return removeContact;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { name, email, phone, id: nanoid() };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact
+};
